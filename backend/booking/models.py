0303d378c5b7020db83296939a2897fdc5add8a2
gabrielteixeira021 <charlesgabriel102@gmail.com>
2026-01-28 20:18:39 -0300

    Add validation and automatic end time calculation to Appointment model

--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from django.db import models
 from django.conf import settings
+from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
@@ -30,7 +32,38 @@ class Appointment(models.Model):
     status = models.CharField(_("status"), max_length=10, choices=STATUS_CHOICES, default="pending")
     
     created_at = models.DateTimeField(_("data de criação do serviço"), auto_now=False, auto_now_add=True)
-    
+
+    def clean(self):
+        from django.utils import timezone
+        if self.start_time and self.start_time < timezone.now():
+            raise ValidationError("Não é possível agendar no passado")
+        
+        if self.end_time and self.end_time <= self.start_time:
+            raise ValidationError("Horário final deve ser depoiis do inicial")
+
+        # Valida se há confilto de horário
+        qs = Appointment.objects.filter(service=self.service)
+        if self.pk:
+            qs = qs.exclude(pk=self.pk)
+
+        # condição de overlap(sobreposição): start < outro_end e end > outro_start | quando o tempo de um serviço coincide com o de outro
+        overlap = qs.filter(
+            start_time__lt=self.end_time,
+            end_time__gt=self.start_time
+        ).exists()
+
+        if overlap:
+            raise ValidationError("Já existe um agendamento nesse horário")
+        
+        def save(self, *args, **kwargs):
+            # calcula end_time automaticamente baseado na duração do serviço
+            if self.service and self.start_time and not self.end_time:
+                self.end_time = self.start_time + timedelta(
+                    minutes=self.service.duration_minutes
+                )
+            super().save(*args, **kwargs)
+
+
     class Meta:
         ordering = ["-start_time"]
         
